4f84ca208d3dafadf481466bc8c87b03a1c06548
Asadbek <asadbekshodiyev2002@gmail.com>
2025-12-01 00:08:11 +0500

    11-masalagacha tayyor

---
+++ script.js
@@ -0,0 +1,112 @@
+// 1-masala
+console.log("1-masala");
+let k = 35;
+let n = 15;
+for (let i = 1; i <= n; i++) {
+  console.log(k);
+}
+
+// 2-masala
+console.log("");
+console.log("2-masala");
+let a = 4;
+let b = 8;
+for (let i = a; i <= b; i++) {
+  console.log(i);
+}
+console.log("Chiqarilgan sonlar:", b - a + 1);
+
+// 3-masala
+console.log("");
+console.log("3-masala");
+let a1 = 10;
+let b1 = 20;
+for (let i = a1 + 1; i < b1; i++) {
+  console.log(i);
+}
+console.log("Chiqarilgan sonlar:", b1 - a1 - 1);
+
+// 4-masala
+console.log("");
+console.log("4-masala");
+let kanfet = 20000;
+for (let i = 1; i <= 10; i++) {
+  let narx = kanfet * i;
+  console.log(`${i} kg konfet narxi: ${narx} so'm`);
+}
+
+// 5-masala
+console.log("");
+console.log("5-masala");
+let kanfet1 = 12000;
+for (let i = 0.1; i <= 1; i = i + 0.1) {
+  let narx = kanfet1 * i;
+  console.log(`${i.toFixed(2)} kg konfet narxi: ${narx.toFixed(2)} so'm`);
+}
+
+// 6-masala
+console.log("");
+console.log("6-masala");
+let kanfet2 = 35000;
+for (let i = 1; i <= 2; i = i + 0.2) {
+  let narx = kanfet2 * i;
+  console.log(`${i.toFixed(1)} kg konfet narxi: ${narx.toFixed(2)} so'm`);
+}
+
+// 7-masala
+console.log("");
+console.log("7-masala");
+let a2 = 12;
+let b2 = 30;
+let yigindi = 0;
+for (let i = a2; i < b2; i++) {
+  yigindi = yigindi + i;
+}
+console.log("Yig'indi:", yigindi);
+
+// 8-masala
+console.log("");
+console.log("8-masala");
+let a3 = 1;
+let b3 = 5;
+let kopaytmasi = 1;
+for (let i = a3; i <= b3; i++) {
+  kopaytmasi = kopaytmasi * i;
+}
+console.log("Ko'paytmasi:", kopaytmasi);
+
+// 9-masala
+console.log("");
+console.log("9-masala");
+let a4 = 1;
+let b4 = 5;
+let kvadrati = 0;
+for (let i = a4; i <= b4; i++) {
+  kvadrati = kvadrati + i * i;
+}
+console.log("Kvadrati yig'indisi:", kvadrati);
+
+// 10-masala
+console.log("");
+console.log("10-masala");
+let n1 = 8;
+let S = 0;
+for (let i = 1; i <= n1; i++) {
+  S = S + 1 / i;
+}
+console.log("S =", S.toFixed(2));
+
+// 11-masala
+console.log("");
+console.log("11-masala");
+let n2 = 25;
+let S2 = 0;
+for (let i = 1; i <= n2; i = i + 1) {
+  S2 = S2 + i * i;
+}
+console.log("S2 =", S2);
+
+
+// 12-masala
+console.log("");
+console.log("12-masala");
